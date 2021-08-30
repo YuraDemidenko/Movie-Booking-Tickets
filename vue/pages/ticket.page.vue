2f2dd2a024e088c351b17ddc8a31ea8f73cc3ed4
YuraDemidenko <83308882+YuraDemidenko@users.noreply.github.com>
2021-08-30 20:54:28 +0300

    Update ticket.page.vue

--- vue/pages/ticket.page.vue
+++ vue/pages/ticket.page.vue
@@ -1,57 +1,85 @@
 <template>
-    <div class="ticket-box">
+    <div class="ticket-page">
 
-        <div class="promo-container">
+        <div class="ticket-page-bg">
+            <img :src="movie.poster">
+            <div class="ticket-page-overlay"></div>
+        </div>
+      
+        <div class="session-info-box">
+            <div class="movies-info">
+                <div class="movie-poster-wrapper">
+                    <div class="session-poster">
+                        <img :src="movie.poster">
+                    </div>
+                    <div class="session-info">
+                        <div class="movie-title">
+                            <p>{{movie.title}}</p>
+                        </div>
+                        <div class="movie-genre">
+                            <p>{{'Genre:  ' + movie.genre}}</p>
+                        </div>
+                        <div class="movie-duration">
+                            <p>{{'Duration:  ' + movie.duration}}</p>
+                        </div>
+                        <div class="movie-ticket-price">
+                            <p>{{'Price:  ' + movie.price}}</p>
+                        </div>
+                        <div class="movie-begining">
+                            <p>{{'Begin Of Session:  ' + movie.beginOfsession}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="movie-discription">
+                    <p>{{movie.discription}}</p>
+                </div>
 
-            <div class="promo-ticket-wrapper">
+                <div class="places-box">
+                   <div class="screen"></div>
 
-                <div class="discription">
 
-                    <div class="poster">
-                        <img :src="movies[index].poster">
+                    <div class="places-scheme">
+
+                        <div class="row" v-for="(row, index) in movie.places" :key="index">
+                            <div class="row-number">{{'Ряд    ' + index}}</div>
+                            <div class="place" :class="{'isReserved': place.isReserved}" v-for="(place, index) in row" :key="index" @click="getPlace(place)">
+                                <div class="place-number" :class="{'active': place.isReserved}">{{place.place}}</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="place-info-box">
+                    <div class="place-status">
+                        <div class="color-anotation">
+                            <span class="round" style="background: #A55EEA"></span>
+                            <h3>Место свободно</h3>
+                        </div>
+                        <div class="color-anotation">
+                            <span class="round" style="background: #4CD137"></span>
+                            <h3>Вы выбрали это место </h3>
+                        </div>
+                        <!-- <div class="color-anotation">
+                            <span class="round" style="background: #383838"></span>
+                            <h3>Место занято</h3>
+                        </div> -->
                     </div>
 
-                    <table>
-                        <thead></thead>
-                        <tbody>
-                            <tr>
-                                <td>{{'Title: ' + movies[index].title}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{'Genre: ' + movies[index].genre}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{'Duration: ' + movies[index].duration + ' min'}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{'Ticket Price: ' + movies[index].price}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{'Begin of Session: ' + movies[index].beginOfsession}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    
-                </div>
+                    <div class="bought-tickets">
+                        <div class="basket-box">
+                            <div class="basket-title">Мои билеты</div>
+                            <div class="total-summ">
+                                <!-- <div class="summ-title">Итого: 200грн</div> -->
+                                <div class="summ-title">Will see you in Theatre</div>
+                                <button class="buy-btn" @click="buyTicket">Buy Ticket</button>
+                            </div>
+                        </div>
+                    </div>
 
-                <div class="about-movie">
-                    <div class="movie-discription">{{movies[index].discription}}</div>  
                 </div>
-
-                <div class="hall-scheme">
-                    <div class="screen"></div>
-                    <table>
-                        <tbody>
-                            <tr v-for="(item, index) in this.movies[this.index].places" :key="index">
-                                <td class="place" :class="{'active': item.isReserved}" v-for="(item, index) 
-                                in item" :key="index"  v-on:click="getPlace(item)">{{index + 1}}</td>  
-                            </tr> 
-                        </tbody>
-                    </table>
-                    <div class="buy-ticket-btn" v-on:click='buyTicket'>Buy Ticket</div>
-                </div>
-                
             </div>
+            
         </div>
         
     </div>
@@ -65,28 +93,32 @@
             return{
                 ticket: '',
                 ticketPlace: '',
-                ticketRow: ''
+                ticketRow: '',
             }
         },
 
         methods: {
-            getPlace: function(item) {
-                item.isReserved = !item.isReserved;
+            getPlace: function(place) {
+                place.isReserved = !place.isReserved;
             },
 
             buyTicket: function() {
-                this.$router.push({name: 'client'});
+                // this.$router.push({name: 'client'});
+
+                console.log(this.movie.price);
             }
         },
         
 
         computed: {
-            index: function() {
+
+            movie: function() {
                 return this.$store.getters['selectedMovie'];
             },
-            movies: function() {
-                return this.$store.getters['movies'];
-            }
+            // movies: function() {
+            //     return this.$store.getters['movies'];
+            // },
+
         },
         
         
