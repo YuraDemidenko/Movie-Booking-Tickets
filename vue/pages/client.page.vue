0e21d788e439472d8e859ae7f31103a4644fec5e
YuraDemidenko <83308882+YuraDemidenko@users.noreply.github.com>
2021-08-30 20:54:03 +0300

    Update client.page.vue

--- vue/pages/client.page.vue
+++ vue/pages/client.page.vue
@@ -5,13 +5,14 @@
             <div class="cinema-app-header">
                 <div class="cinema-app-nav-bar">
                     <div class="genre-filter">
-                        <div class="genre-filter-btn" v-on:click="filter('comedy')">Comedy</div>
-                        <div class="genre-filter-btn" v-on:click="filter('action')">Action</div>
-                        <div class="genre-filter-btn" v-on:click="filter('drama')">Drama</div>
-                        <div class="genre-filter-btn" v-on:click="filter('crime')">Crime</div>
-                        <div class="genre-filter-btn" v-on:click="filter('')">All Movies</div>
+                        <button class="genre-filter-btn" v-on:click="filter('comedy')">Comedy</button>
+                        <button class="genre-filter-btn" v-on:click="filter('action')">Action</button>
+                        <button class="genre-filter-btn" v-on:click="filter('drama')">Drama</button>
+                        <button class="genre-filter-btn" v-on:click="filter('crime')">Crime</button>
+                        <button class="genre-filter-btn" v-on:click="filter('')">All Movies</button>
                     </div>
                     <div class="movie-search">
+                        <img src="../img/search.svg" alt="">
                         <input type="text" class="search" v-model="query"> 
                     </div>
                 </div>
@@ -21,9 +22,11 @@
                 <div class="movie-list">
                     <div class="list-wrapper" v-if="filteredMovies.length">
 
+                        <h2 class="movie-list-title">Movies</h2>
+
                         <router-link :to="{name: 'ticket-page'}">
                             <transition-group name="scale">
-                                <div class="promo-box" v-for="(movie, index) in filteredMovies" :key="index" v-on:click="getIndex(index)">
+                                <div class="promo-box" v-for="(movie, index) in filteredMovies" :key="index" v-on:click="getMovie(movie)">
                                     <div class="promo-title">
                                         <img :src="movie.poster">
                                         <p>{{movie.title}}</p>
@@ -72,8 +75,10 @@
                 this.genre = genre;     
             },
 
-            getIndex: function(index) {
-                this.$store.commit('setIndex', index); 
+            getMovie: function(movie) {
+                // this.$store.commit('setIndex', index);  
+                this.$store.commit('seeMovieInfo', movie);  
+                // console.log(movie);
             }
             
 
